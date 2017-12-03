b839659796f1583e598d5717bf7ca1c0d8448385
leoeberts <leonardo.eberts@gmail.com>
2017-12-03 18:54:25 -0200

    Correcting service port on tests

--- src/test/java/com/taskboards/serviceregistry/ServiceRegistryApplicationTest.java
+++ src/test/java/com/taskboards/serviceregistry/ServiceRegistryApplicationTest.java
@@ -16,19 +16,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest(classes = ServiceRegistryApplicationTest.class)
+@SuppressWarnings("rawtypes")
 public class ServiceRegistryApplicationTest {
 	
 	@Test
 	public void catalogLoads() throws Exception {
-		@SuppressWarnings("rawtypes")
-		ResponseEntity<Map> entity = new TestRestTemplate("user", "password").getForEntity("http://localhost:" + 8761 + "/eureka/apps", Map.class);
+		ResponseEntity<Map> entity = new TestRestTemplate("user", "password").getForEntity("http://localhost:8761/eureka/apps", Map.class);
 		assertEquals(HttpStatus.OK, entity.getStatusCode());
 	}
 
 	@Test
 	public void adminLoads() {		
-		@SuppressWarnings("rawtypes")
-		ResponseEntity<Map> entity = new TestRestTemplate("user", "password").getForEntity("http://localhost:" + 8761 + "/env", Map.class);
+		ResponseEntity<Map> entity = new TestRestTemplate("user", "password").getForEntity("http://localhost:8761/env", Map.class);
 		assertEquals(HttpStatus.OK, entity.getStatusCode());
 	}
 	
